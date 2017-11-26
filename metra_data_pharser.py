7bb96ccf63a87f407faca34dddb9e99f3552b6eb
justindarcy <33602750+justindarcy@users.noreply.github.com>
2017-11-25 21:31:38 -0600

    Update metra_data_pharser.py
    adding static schedule function for when live data does not exist

--- metra_data_pharser.py
+++ metra_data_pharser.py
@@ -1,69 +1,128 @@
-import requests, json
+# -*- coding: utf-8 -*-
+import requests, json, calendar
 from datetime import datetime
 from dateutil import tz
-"""
-realtime_trips_updates_input is the input to the file
-station_id is your station of choice
-"""
-# fething the data here
-response = requests.get('https://gtfsapi.metrarail.com/gtfs/tripUpdates', auth=(
-'f0db64a9715bf9d74470f5587b8750b2', 'd3ca32f24b4868e11c90b43fd9bb6589'))
 
-python_object = json.loads(response.content.decode("utf-8"))
+# ---------------------fething the data here------------------------
+response = requests.get('https://gtfsapi.metrarail.com/gtfs/schedule/stop_times', auth=(
+'f0db64a9715bf9d74470f5587b8750b2', 'd3ca32f24b4868e11c90b43fd9bb6589'))
+static_schedule_input = json.loads(response.content.decode("utf-8"))
 
 if response.status_code == 200:
-	print "\nData access from Metra API successfull \n"
+	print ("\nData access from Metra API successfull \n")
 else:
     print ("\n API data access failed\n")
 
-realtime_trips_updates_input = python_object
+response2 = requests.get('https://gtfsapi.metrarail.com/gtfs/schedule/trips', auth=('f0db64a9715bf9d74470f5587b8750b2', 'd3ca32f24b4868e11c90b43fd9bb6589'))
+trips_api = json.loads(response2.content.decode("utf-8"))
 
-station_id = "RAVENSWOOD"
-stop_number = 3  # stop_number is the number of stops from OTC, when OTC=1
-# WHen stop number is low, train is going North, when high, train going south
+response3 = requests.get('https://gtfsapi.metrarail.com/gtfs/schedule/calendar', auth=('f0db64a9715bf9d74470f5587b8750b2', 'd3ca32f24b4868e11c90b43fd9bb6589'))
+calendar_api = json.loads(response3.content.decode("utf-8"))
 
-for x in range(0,len(realtime_trips_updates_input)):
+# ---------------------fething the data here------------------------
 
-    for key, value in realtime_trips_updates_input[x]['trip_update'].iteritems() :
+# ----- input variables-----------
+my_station_id = "RAVENSWOOD" # station you leave from
+stop_number = 3  # stops away from OTC your station is
+desired_direction="North"  # direction from above to below
+destination = "EVANSTON" # station you go to
+# ----- input variables-----------
 
-        if key == 'stop_time_update':
+direction='blank'
+train_times=[]
+future_train_times=[]
+train_options_trip_id=[]
+train_options_filtered=[]
+next_train_id=[]
 
-            for v in value:  # itterating through the values list
-                #print type(v)
-                for key, value in v.iteritems():
-                    #print key
-                    if value == station_id:
-                        my_station_trains = v
-                        #print v  # this is for debugging purposes
+# ------calendar variables---------
+day_names = list(calendar.day_name) 
+today_num=(datetime.today().weekday()) #Monday is 0 and Sunday is 6
+today = (day_names[today_num].lower())
+service_id_list=[]
+# ------calendar variables---------
 
-try:
-    for key, value in my_station_trains.iteritems():
-        if key == "stop_sequence":
-            #print key, value
-            if value <= stop_number:
-                print ("Train is going North")
-            elif value > stop_number:
-                print ("Train is going South")
-        elif key == 'arrival':
-            #print key, value
-            for k, v in value.iteritems():
-                #print k, v
-                if k == "delay":
-                    print "delay=", v
-                elif k == "time":
-                    #print "arrival time =", v['low']
-                    from_zone = tz.tzutc()
-                    to_zone = tz.tzlocal()
-                    # utc = datetime.utcnow()
-                    utc = datetime.strptime(v['low'], '%Y-%m-%dT%H:%M:%S.000Z')
-                    # Tell the datetime object that it's in UTC time zone since
-                    # datetime objects are 'naive' by default
-                    utc = utc.replace(tzinfo=from_zone)
-                    # Convert time zone
-                    central = utc.astimezone(to_zone)
-                    print("arrival time =", central.strftime("%I:%M %p"))
+def service_finder():   
+    for cal in calendar_api:
+        for key, value in cal.items():
+    
+            if key == today and value==1:
+                service_id_list.append(cal['service_id'])
+    return(service_id_list)
 
-except NameError:
-    # here i want to access the static schedule and return the next
-    # scheduled train and have google assistant mention that the data isn't live
-    print "No live data for your scheduled stop"
+
+def service_id_lookup(trip_id_str):
+    for trip in trips_api:
+        if trip['trip_id'] == trip_id_str:
+            return (trip['service_id'])
+
+
+def trip_lookup(t_id):
+    for t_list in static_schedule_input:
+        if t_list['trip_id'] == t_id and t_list['stop_id'] == destination:
+
+            service_id_str = service_id_lookup(t_list['trip_id'])
+            service_id_list = service_finder()
+            if service_id_str in service_id_list:         
+                # trains stopping at ravenswood, going north, and stopping at destination for today
+                train_options_trip_id.append(t_list['trip_id'])
+    return train_options_trip_id
+
+
+for d in static_schedule_input:
+    if d['stop_id'] == my_station_id:
+
+        if d['stop_sequence'] > stop_number:
+            direction='South'
+        else:
+            direction='North'
+            
+        if direction == desired_direction:
+            train_options_trip_id = trip_lookup(d['trip_id'])
+
+            if d['arrival_time'] not in train_times and d['trip_id'] in train_options_trip_id:
+                train_times.append(d['arrival_time'])
+                train_options_filtered.append(d['trip_id'])
+                
+train_times_sort = sorted(train_times)
+#print(train_times_sort, '\n')
+now = datetime.now()
+c=0
+for t in train_times_sort:
+    if t[0:2] == '24':
+        t_new = datetime.strptime(t, '24:%M:%S')
+        # add tomorrows year-month-day, below (+1) likely doesn't work
+        t_new = t_new.replace(year=now.year, month=now.month, day=now.day+1)
+        if t_new>now:
+            future_train_times.append(t_new)
+    else:
+        t_new = datetime.strptime(t, '%H:%M:%S')
+        t_new = t_new.replace(year=now.year, month=now.month, day=now.day)
+        if t_new>now:
+            future_train_times.append(t_new)
+            next_train_id.append(train_options_filtered[c])
+    c=+1
+
+
+next_train = future_train_times[0]
+print('next_train(scheduled)=', next_train.strftime("%I:%M %p"))
+#print(next_train_id[0])
+
+# fething the live data here
+response = requests.get('https://gtfsapi.metrarail.com/gtfs/tripUpdates', auth=(
+'f0db64a9715bf9d74470f5587b8750b2', 'd3ca32f24b4868e11c90b43fd9bb6589'))
+realtime_trips_updates_input = json.loads(response.content.decode("utf-8"))
+
+for dic in realtime_trips_updates_input:
+    if dic['id'] == next_train_id[0]:
+        
+        my_delay = dic['trip_update']['stop_time_update']['arrival']['delay']
+        
+        
+my_delay_exists = 'my_delay' in locals() or 'var' in globals()
+
+if my_delay_exists:
+    print('Live data exists')
+    print('delay=',my_delay)
+else:
+    print('No live data')
